b04992c7b6fd19c5324d0ec18a727c34c2a9cfad
Magasoff <magasovramil@yandex.ru>
2023-01-12 00:11:08 +0300

    2 задание.

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,34 @@
+import java.util.Objects;
+
 public class Car {
-    String brand; // марка автомобиля
-    String model; // модель автомобиля
-    double engineVolume; // двигатель объем
-    String color; // цвет автомобиля
-    int year; // год выпуска автомобиля
-    String country; // страна сборки автомобиля
+    private String brand; // марка автомобиля
+    private String model; // модель автомобиля
+    private double engineVolume; // двигатель объем
+    private String color; // цвет автомобиля
+    private int year; // год выпуска автомобиля
+    private String country; // страна сборки автомобиля
+
+    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return " Марка автомобиля: " + brand + " Модель автомобиля: " + model + " объем двигателя: " + engineVolume +
+                " Цвет автомобиля: " + color + " год выпуска автомобиля: " + year + " страна сборки автомобиля: " + country;
+    }
 }
+
+
+
+
+
+
+
+
+

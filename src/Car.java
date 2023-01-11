ebb48fdcedd44fe50c31f13faccc892a9c3b484d
Magasoff <magasovramil@yandex.ru>
2023-01-11 23:32:43 +0300

    1 задание.

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String brand; // марка автомобиля
+    String model; // модель автомобиля
+    double engineVolume; // двигатель объем
+    String color; // цвет автомобиля
+    int year; // год выпуска автомобиля
+    String country; // страна сборки автомобиля
+}
